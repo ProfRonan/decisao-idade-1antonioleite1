d5f621b9cec677e7a0ee219a7f5810a634eec4c8
Ronan Pardo Soares <ronan.soares@gmail.com>
2023-05-15 15:59:06 -0300

    Fix change of tests

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ maior = False
 velho = False
 print (idade)
 if idade < 0:
-    print('impossível!')
-    print('não precisa se alistar.')
+    print('Impossível!')
+    print('Não precisa se alistar.')
     impossível = True
 elif idade < 18: 
-    print('não precisa se alistar.')
+    print('Não precisa se alistar.')
     menor = True
 elif 18 < idade < 65: # 18 < idade and idade < 65
     print('Não esqueça de votar na próxima eleição.')
@@ -20,4 +20,4 @@ elif idade > 65:
     print ('Vá descansar.')
     velho = True
 elif not (impossível or menor or maior or velho) :
-    print("eita!")
+    print("Eita!")
